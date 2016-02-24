b1b670df37fea6f6c397d7d5b380cf78d2159351
leonid <vasiliev.leonid@gmail.com>
2016-02-24 09:55:57 +0300

    more

--- init.sh
+++ init.sh
@@ -1,1 +1,9 @@
+
+
+sudo apt-key add nginx_signing.key
+echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/sources.list
+echo "deb-src http://nginx.org/packages/ubuntu/ trusty nginx" >> /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get install nginx
 sudo ln -s /home/box/web/etc/nginx.conf /etc/nginx/test.conf
+
